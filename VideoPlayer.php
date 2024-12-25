9a1b18b162f6b0964db1bf2d370e6b3ad28b32f1
VokanicsKristof <crystalosdev@gmail.com>
2024-12-25 23:24:14 +0100

    Adding favorites (Part I)

--- VideoPlayer.php
+++ VideoPlayer.php
@@ -513,124 +513,122 @@ echo "<!DOCTYPE html>
     <body>
         <div class=\"modal fade show\" id=\"myModal\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"exampleModalLabel\" aria-modal=\"true\" >
             <div class=\"modal-dialog\" role=\"document\">
-            <div class=\"modal-content\">
+                <div class=\"modal-content\">
+                    <div class=\"modal-body\">
 
-                <div class=\"modal-body\">
-
-                <button type=\"button\" class=\"close\" id=\"closeModalBtn\" data-dismiss=\"modal\" aria-label=\"Close\">
-                    <span aria-hidden=\"true\">×</span>
-                </button>
-                <!-- 16:9 aspect ratio -->
-                    <div class=\"videoiframe\">
-                            ";
-                            if (strpos($Film_Link, 'm3u8') !== false) {
-                                echo "<video-js style=\"width: 100%; height: 100%;\" id=\"my-video\" class=\"video-js vjs-default-skin embed-responsive-item\" controls preload=\"auto\" width=\"80%\" height=\"80%\" data-setup='{}'>
-                                <source src=\"{$Film_Link}\" type=\"application/x-mpegURL\">
-                                </video-js>";
-                            } else {
-                                echo "<iframe src=\"{$Film_Link}\" class=\"embed-responsive-item\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>";
-                            }
-                            echo "       </div>
-                </div>
-
-            </div>
-            </div>
-        </div>
-            <div class=\"page-content\">
-                <div class=\"background\"></div>
-                <div class=\"header\">
-                    <img src=\"logo.svg\" alt=\"MovieFlix Logo\" class=\"logo\">
-                    <div class=\"menu\">
-                        <div class=\"menu-item\">
-                            <a href=\"index.html\" style=\"text-decoration: none; color: inherit;\">Főoldal</a>
-                        </div>
-                        <div class=\"menu-item\">Legutóbbiak</div>
-                        <div class=\"menu-item\">Kedvencek</div>
-                        <div class=\"menu-item\">Profilom</div>
-                        <div class=\"menu-item\">Beállítások</div>
+                    <button type=\"button\" class=\"close\" id=\"closeModalBtn\" data-dismiss=\"modal\" aria-label=\"Close\">
+                        <span aria-hidden=\"true\">×</span>
+                    </button>
+                    <!-- 16:9 aspect ratio -->
+                        <div class=\"videoiframe\">
+                                ";
+                                if (strpos($Film_Link, 'm3u8') !== false) {
+                                    echo "<video-js style=\"width: 100%; height: 100%;\" id=\"my-video\" class=\"video-js vjs-default-skin embed-responsive-item\" controls preload=\"auto\" width=\"80%\" height=\"80%\" data-setup='{}'>
+                                    <source src=\"{$Film_Link}\" type=\"application/x-mpegURL\">
+                                    </video-js>";
+                                } else {
+                                    echo "<iframe src=\"{$Film_Link}\" class=\"embed-responsive-item\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>";
+                                }
+            echo "       </div>
                     </div>
                 </div>
-                <div class=\"video-player\">
-                    <video id=\"trailer\" autoplay muted loop playsinline src=\"{$IMDB_Elozetes}\" style=\"max-width: 100%\"></video>
-                    <div id=\"no-trailer-message\" style=\"display: none; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: rgba(0, 0, 0, 0.8); color: white; padding: 20px; font-size: 35px; border-radius: 10px; text-align: center;\">Nincs elérhető előzetes</div>
-                </div>
-                <div class=\"bottom-bar\">
-                    <div class=\"poster-frame\">
-                        <img src=\"{$Film_Boritokep}\" alt=\"{$Film_Cim}\">
-                    </div>
-                    <div class=\"content\">
-                        <div class=\"film-title\">{$Film_Cim}</div>
-                        <div class=\"videoMetadata--second-line\">
-                            <div class=\"year\">{$Film_Megjelenes}</div>
-                            <span class=\"duration\">{$Film_Hossz} h</span>
-                            <span class=\"player-feature-badge\">HD</span>
-                            <div class=\"ltr-bjn8wh\">
-                                <div class=\"ltr-x1hvkl\" style=\"display: flex;flex-direction: row;justify-content: center;align-content: flex-end;flex-wrap: nowrap;align-items: center;gap: 10px;\">
-                                    <div aria-labelledby=\"standaloneAudioDescriptionAvailable\" data-tooltip=\"Audio description is available\">
-                                    </div>
-                                        <svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" role=\"img\" viewBox=\"0 0 24 24\" width=\"34\" height=\"34\" data-icon=\"AudioDescriptionStandard\" aria-hidden=\"true\" class=\"ltr-18tpq4v\">
-            <path fill-rule=\"evenodd\" clip-rule=\"evenodd\" d=\"M21.9782 7.52002H22.2621C23.37 8.81831 24.0001 10.4801 24.0001 12.2077C24.0001 13.7414 23.505 15.2301 22.6221 16.4453H22.3348H21.8501H21.5662C22.5598 15.2613 23.1207 13.7691 23.1207 12.2077C23.1207 10.449 22.404 8.75599 21.1611 7.52002H21.445H21.9782ZM6.91381 16.4796H8.87336V7.52661H6.42566L0 16.4796H2.87701L3.63174 15.2956H6.91381V16.4796ZM4.8625 13.4299H6.92592V10.224L4.8625 13.4299ZM12.3019 9.62283C13.621 9.62283 14.6839 10.6926 14.6839 12.0048C14.6839 13.3203 13.621 14.3901 12.3019 14.3901H11.6787V9.62283H12.3019ZM12.5443 16.4743C15.0128 16.4743 17.0208 14.4698 17.0208 12.0048C17.0208 9.52935 15.0335 7.52826 12.565 7.52826H12.5373H9.79883V16.4778H12.5443V16.4743ZM20.0103 7.52002H19.7264H19.1932H18.9093C20.1522 8.75599 20.8689 10.449 20.8689 12.2077C20.8689 13.7691 20.308 15.2613 19.3144 16.4453H19.5983H20.083H20.3634C21.2531 15.2301 21.7482 13.7414 21.7482 12.2077C21.7482 10.4801 21.1181 8.81831 20.0103 7.52002ZM17.4745 7.52002H17.7584C18.8663 8.81831 19.4895 10.4801 19.4895 12.2077C19.4895 13.7414 19.0013 15.2301 18.1116 16.4453H17.8277H17.3464H17.0625C18.0492 15.2613 18.6101 13.7691 18.6101 12.2077C18.6101 10.449 17.9004 8.75599 16.6575 7.52002H16.9344H17.4745Z\" fill=\"currentColor\"></path>
-        </svg>
-        <div aria-labelledby=\"standaloneTextClosedCaptionsAvailable\" data-tooltip=\"Subtitles for the deaf and hard of hearing are available\">
-            <svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" role=\"img\" viewBox=\"0 0 16 16\" width=\"20\" height=\"20\" data-icon=\"SubtitlesSmall\" aria-hidden=\"true\">
-                <path fill-rule=\"evenodd\" clip-rule=\"evenodd\" d=\"M0 1.75C0 1.33579 0.335786 1 0.75 1H15.25C15.6642 1 16 1.33579 16 1.75V12.25C16 12.6642 15.6642 13 15.25 13H12.75V15C12.75 15.2652 12.61 15.5106 12.3817 15.6456C12.1535 15.7806 11.8709 15.785 11.6386 15.6572L6.80736 13H0.75C0.335786 13 0 12.6642 0 12.25V1.75ZM1.5 2.5V11.5H7H7.19264L7.36144 11.5928L11.25 13.7315V12.25V11.5H12H14.5V2.5H1.5ZM6 6.5L3 6.5V5L6 5V6.5ZM13 7.5H10V9H13V7.5ZM3 9V7.5L9 7.5V9L3 9ZM13 5H7V6.5H13V5Z\" fill=\"currentColor\"></path>
-            </svg>
+            </div>
         </div>
+        <div class=\"page-content\">
+            <div class=\"background\"></div>
+            <div class=\"header\">
+                <img src=\"logo.svg\" alt=\"MovieFlix Logo\" class=\"logo\">
+                <div class=\"menu\">
+                    <div class=\"menu-item\">
+                        <a href=\"index.html\" style=\"text-decoration: none; color: inherit;\">Főoldal</a>
+                    </div>
+                    <div class=\"menu-item\">Legutóbbiak</div>
+                    <div class=\"menu-item\">Kedvencek</div>
+                    <div class=\"menu-item\">Profilom</div>
+                    <div class=\"menu-item\">Beállítások</div>
+                </div>
+            </div>
+            <div class=\"video-player\">
+                <video id=\"trailer\" autoplay muted loop playsinline src=\"{$IMDB_Elozetes}\" style=\"max-width: 100%\"></video>
+                <div id=\"no-trailer-message\" style=\"display: none; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: rgba(0, 0, 0, 0.8); color: white; padding: 20px; font-size: 35px; border-radius: 10px; text-align: center;\">Nincs elérhető előzetes</div>
+            </div>
+            <div class=\"bottom-bar\">
+                <div class=\"poster-frame\">
+                    <img src=\"{$Film_Boritokep}\" alt=\"{$Film_Cim}\">
+                </div>
+                <div class=\"content\">
+                    <div class=\"film-title\">{$Film_Cim}</div>
+                    <div class=\"videoMetadata--second-line\">
+                        <div class=\"year\">{$Film_Megjelenes}</div>
+                        <span class=\"duration\">{$Film_Hossz} h</span>
+                        <span class=\"player-feature-badge\">HD</span>
+                        <div class=\"ltr-bjn8wh\">
+                            <div class=\"ltr-x1hvkl\" style=\"display: flex;flex-direction: row;justify-content: center;align-content: flex-end;flex-wrap: nowrap;align-items: center;gap: 10px;\">
+                                <div aria-labelledby=\"standaloneAudioDescriptionAvailable\" data-tooltip=\"Audio description is available\">
+                                </div>
+                                    <svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" role=\"img\" viewBox=\"0 0 24 24\" width=\"34\" height=\"34\" data-icon=\"AudioDescriptionStandard\" aria-hidden=\"true\" class=\"ltr-18tpq4v\">
+                                        <path fill-rule=\"evenodd\" clip-rule=\"evenodd\" d=\"M21.9782 7.52002H22.2621C23.37 8.81831 24.0001 10.4801 24.0001 12.2077C24.0001 13.7414 23.505 15.2301 22.6221 16.4453H22.3348H21.8501H21.5662C22.5598 15.2613 23.1207 13.7691 23.1207 12.2077C23.1207 10.449 22.404 8.75599 21.1611 7.52002H21.445H21.9782ZM6.91381 16.4796H8.87336V7.52661H6.42566L0 16.4796H2.87701L3.63174 15.2956H6.91381V16.4796ZM4.8625 13.4299H6.92592V10.224L4.8625 13.4299ZM12.3019 9.62283C13.621 9.62283 14.6839 10.6926 14.6839 12.0048C14.6839 13.3203 13.621 14.3901 12.3019 14.3901H11.6787V9.62283H12.3019ZM12.5443 16.4743C15.0128 16.4743 17.0208 14.4698 17.0208 12.0048C17.0208 9.52935 15.0335 7.52826 12.565 7.52826H12.5373H9.79883V16.4778H12.5443V16.4743ZM20.0103 7.52002H19.7264H19.1932H18.9093C20.1522 8.75599 20.8689 10.449 20.8689 12.2077C20.8689 13.7691 20.308 15.2613 19.3144 16.4453H19.5983H20.083H20.3634C21.2531 15.2301 21.7482 13.7414 21.7482 12.2077C21.7482 10.4801 21.1181 8.81831 20.0103 7.52002ZM17.4745 7.52002H17.7584C18.8663 8.81831 19.4895 10.4801 19.4895 12.2077C19.4895 13.7414 19.0013 15.2301 18.1116 16.4453H17.8277H17.3464H17.0625C18.0492 15.2613 18.6101 13.7691 18.6101 12.2077C18.6101 10.449 17.9004 8.75599 16.6575 7.52002H16.9344H17.4745Z\" fill=\"currentColor\"></path>
+                                    </svg>
+                                    <div aria-labelledby=\"standaloneTextClosedCaptionsAvailable\" data-tooltip=\"Subtitles for the deaf and hard of hearing are available\">
+                                        <svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" role=\"img\" viewBox=\"0 0 16 16\" width=\"20\" height=\"20\" data-icon=\"SubtitlesSmall\" aria-hidden=\"true\">
+                                            <path fill-rule=\"evenodd\" clip-rule=\"evenodd\" d=\"M0 1.75C0 1.33579 0.335786 1 0.75 1H15.25C15.6642 1 16 1.33579 16 1.75V12.25C16 12.6642 15.6642 13 15.25 13H12.75V15C12.75 15.2652 12.61 15.5106 12.3817 15.6456C12.1535 15.7806 11.8709 15.785 11.6386 15.6572L6.80736 13H0.75C0.335786 13 0 12.6642 0 12.25V1.75ZM1.5 2.5V11.5H7H7.19264L7.36144 11.5928L11.25 13.7315V12.25V11.5H12H14.5V2.5H1.5ZM6 6.5L3 6.5V5L6 5V6.5ZM13 7.5H10V9H13V7.5ZM3 9V7.5L9 7.5V9L3 9ZM13 5H7V6.5H13V5Z\" fill=\"currentColor\"></path>
+                                        </svg>
                                     </div>
                                 </div>
-                            </div>  
+                            </div>
+                        </div>  
+                    
+                    <div style=\"display: flex; justify-content: flex-start; align-items: center;\">
+                        <img src=\"https://cdn.siter.io/assets/ast_cSHVq2tCHCdum6h5A4AM6NTSq/3b6eb131-ba57-4e08-9fda-1debe6715a33.webp\"
+                            alt=\"Korhatár\" class=\"korhatar\">
+                        <div class=\"film-description\">A műsorszám megtekintése 16 éven aluliak számára nem ajánlott.</div>
                         
-                        <div style=\"display: flex; justify-content: flex-start; align-items: center;\">
-                            <img src=\"https://cdn.siter.io/assets/ast_cSHVq2tCHCdum6h5A4AM6NTSq/3b6eb131-ba57-4e08-9fda-1debe6715a33.webp\"
-                                alt=\"Korhatár\" class=\"korhatar\">
-                            <div class=\"film-description\">A műsorszám megtekintése 16 éven aluliak számára nem ajánlott.</div>
-                            
-                        </div>
-                        <div class=\"buttons\">
-                            <button class=\"button button-watch\" id=\"openModalBtn\">Megtekintem</button>
-                            <button class=\"button button\" onclick=\"window.location.href=\'' . $IMDB_Elozetes . '\'\">Előzetes megtekintése</button>
-                            <button class=\"button button-add\">Listához adás</button>
-                        </div>
-                        <script>
-                        // Modal elem és gombok referenciája
-                        var modal = document.getElementById('myModal');
-                        var openModalBtn = document.getElementById('openModalBtn');
-                        var closeModalBtn = document.getElementById('closeModalBtn');
+                    </div>
+                    <div class=\"buttons\">
+                        <button class=\"button button-watch\" id=\"openModalBtn\">Megtekintem</button>
+                        <button class=\"button button\" onclick=\"window.location.href=\'' . $IMDB_Elozetes . '\'\">Előzetes megtekintése</button>
+                        <button class=\"button button-add\" id=\"AddtoFavorites\">Listához adás</button>
+                    </div>
+                    <script>
+                    // Modal elem és gombok referenciája
+                    var modal = document.getElementById('myModal');
+                    var openModalBtn = document.getElementById('openModalBtn');
+                    var closeModalBtn = document.getElementById('closeModalBtn');
 
-                        // Gomb kattintás eseménykezelő - modal megnyitása
-                        openModalBtn.addEventListener('click', function() {
-                            modal.style.display = 'block'; // Modal megjelenítése
-                        });
+                    // Gomb kattintás eseménykezelő - modal megnyitása
+                    openModalBtn.addEventListener('click', function() {
+                        modal.style.display = 'block'; // Modal megjelenítése
+                    });
 
-                        // Close gomb kattintás eseménykezelő - modal eltüntetése
-                        closeModalBtn.addEventListener('click', function() {
-                            modal.style.display = 'none'; // Modal elrejtése
-                        });
-                        </script>
+                    // Close gomb kattintás eseménykezelő - modal eltüntetése
+                    closeModalBtn.addEventListener('click', function() {
+                        modal.style.display = 'none'; // Modal elrejtése
+                    });
+                    </script>
+                    
                         
-                            
-                        <div class=\"adatlap\">
-                            <div class=\"default-ltr-cache-kiz1b3 em9qa8x3\">
-                                <div class=\"default-ltr-cache-18fxwnx em9qa8x0\">
-                                    <div class=\"default-ltr-cache-1y7pnva em9qa8x1\">
-                                    
-                                    <span class=\" default-ltr-cache-v92n84 euy28770\">{$Film_leiras}</span></div>
-                                            
-                                    <div class=\" default-ltr-cache-1mulv68 eebk2mu2\" role=\"separator\">
-                                        <div class=\"default-ltr-cache-1k8qwhc eebk2mu0\"></div>  
-                                    </div>
-                                    <div class=\"default-ltr-cache-1wmy9hl ehsrwgm0\">
-                                        <div class=\"default-ltr-cache-eywhmi ehsrwgm1\">
-                                            
-                                    </div>
+                    <div class=\"adatlap\">
+                        <div class=\"default-ltr-cache-kiz1b3 em9qa8x3\">
+                            <div class=\"default-ltr-cache-18fxwnx em9qa8x0\">
+                                <div class=\"default-ltr-cache-1y7pnva em9qa8x1\">
+                                
+                                <span class=\" default-ltr-cache-v92n84 euy28770\">{$Film_leiras}</span></div>
+                                        
+                                <div class=\" default-ltr-cache-1mulv68 eebk2mu2\" role=\"separator\">
+                                    <div class=\"default-ltr-cache-1k8qwhc eebk2mu0\"></div>  
+                                </div>
+                                <div class=\"default-ltr-cache-1wmy9hl ehsrwgm0\">
+                                    <div class=\"default-ltr-cache-eywhmi ehsrwgm1\">
+                                        
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
             
-            <script src=\"https://vjs.zencdn.net/7.11.4/video.min.js\"></script>
+        <script src=\"https://vjs.zencdn.net/7.11.4/video.min.js\"></script>
         <script>
             const bottomBar = document.querySelector(\".bottom-bar\");
 
@@ -660,6 +658,26 @@ echo "<!DOCTYPE html>
                 noTrailerMessage.style.display = \"block\";
                 video.style.display = \"none\";
             }
+        </script>
+
+        <script>
+            document.getElementById('AddtoFavorites').addEventListener('click', function() {
+                var movieID = {$MovieID};
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', 'movies.php?addtoFavorites=' + movieID, true);
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState == 4 && xhr.status == 200) {
+                        const response = xhr.responseText.trim();
+                        const button = document.getElementById('AddtoFavorites');
+                        if (response === 'true') {
+                            button.innerHTML = 'Hozzáadva kedvencekhez';
+                        } else {
+                            button.innerHTML = 'Kedvencekhez adás';
+                        }
+                    }
+                };
+                xhr.send();
+            });
         </script>
     </body>
 </html>";
--- movies.php
+++ movies.php
@@ -18,6 +18,7 @@ $search = isset($_GET['search']) ? '%' . $_GET['search'] . '%' : '%';
 $orderBy = isset($_GET['orderBy']) ? $_GET['orderBy'] : '';
 $genre = isset($_GET['genre']) ? $_GET['genre'] : '';
 $wantRecents = isset($_GET['wantRecents']) ? $_GET['wantRecents'] : '';
+$addtoFavorites = isset($_GET['addtoFavorites']) ? $_GET['addtoFavorites'] : '2006';
 
 $Command = "";
 
@@ -82,6 +83,33 @@ else if($wantRecents != "") {
     // Join recents and links tables to get the most recent movies
     // Order is in reverse chronological order
     $stmt = $conn->prepare("SELECT * FROM recents JOIN links ON recents.movieID = links.id");
+}
+else if($addtoFavorites != "") {
+    // Check if the movie is already in the favorites table
+    $stmt = $conn->prepare("SELECT * FROM favorites WHERE movieID = ?");
+    $stmt->bind_param("i", $addtoFavorites);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+    $movies = $result->fetch_all(MYSQLI_ASSOC);
+
+    if(count($movies) == 0) {
+        // If the movie is not in the favorites table, add it
+        $stmt = $conn->prepare("INSERT INTO favorites (movieID) VALUES (?)");
+        $stmt->bind_param("i", $addtoFavorites);
+        $stmt->execute();
+        echo "true";
+    }
+    else {
+        // If the movie is already in the favorites table, remove it
+        $stmt = $conn->prepare("DELETE FROM favorites WHERE movieID = ?");
+        $stmt->bind_param("i", $addtoFavorites);
+        $stmt->execute();
+        echo "false";
+    }
+    $stmt->close();
+    $conn->close();
+    return;
 }
 else {
     $stmt = $conn->prepare("SELECT * FROM links WHERE movie_title LIKE ? " . ($orderBy != "" ? "ORDER BY " . $orderBy : ""));
